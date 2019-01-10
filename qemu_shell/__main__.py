e3f752184fef63a27c47bfec15a5a57f4d938a71
r.tyminski <r.tyminski@partner.samsung.com>
2019-01-10 16:01:22 +0100

    More changes

--- qemu_shell/__main__.py
+++ qemu_shell/__main__.py
@@ -4,6 +4,7 @@ import lexer as ssp
 import os
 import sys
 import subprocess
+import shlex
 from argparse import ArgumentParser
 from argparse import Action
 
@@ -16,31 +17,27 @@ class PathAction(Action):
         super(PathAction, self).__init__(option_strings, dest, **kwargs)
 
     def __call__(self, parser, namespace, values, option_string=None):
-        print('%r %r %r' % (namespace, values, option_string))
-
-#def export_loader_path(path):
-#    os.environ['QEMU_LD_PREFIX'] = path
-
-#def add_libraries_path(path):
-#    _path = os.environ['LD_LIBRARY_PATH']
-#    _path.append(':')
-#    _path.append(path)
-
-#def add_binary_path(path):
-#    _path = os.environ['PATH']
-#    _path.append(':')
-#    _path.append(path)
+        if option_string == "-p" or option_string == "--binary-path":
+            _path = os.environ['PATH']
+            os.environ['PATH'] = _path + ":" + values
+        elif option_string == "-L" or option_string == "--loader-prefix-path":
+            os.environ['QEMU_LD_PREFIX'] = path
+        elif option_string == "-l" or option_string == "--loader-search-path":
+            _path = os.environ['LD_LIBRARY_PATH']
+            os.environ['LD_LIBRARY_PATH'] = _path + ":" + values
 
 def get_full_path(cmd):
     try:
-        output = subprocess.check_output(['which', cmd], shell=True, stderr=subprocess.STDOUT)
+        output = subprocess.check_output(["which", cmd], universal_newlines=True)
     except subprocess.CalledProcessError as e:
-        print("{}".format(e.output))
-        sys.exit(1)
-    return output
+        return None
+    return output.strip()
 
 def get_arch(cmd):
-    output = subprocess.check_output(['file', '-b', cmd])
+    try:
+        output = subprocess.check_output(["file", "-b", "-L", cmd], universal_newlines=True)
+    except subprocess.CalledProcessError as e:
+        return None
     properties = output.split(',')
     return properties[1]
 
@@ -62,17 +59,29 @@ def main(stream=None):
     parser.add_argument('-v', '--version',
             action='version', version='%(prog)s 0.1')
 
-    command = None
+    args = parser.parse_args()
+
+    final_command = ""
     while True:
         command = input("qemu-shell$ ")
         if command.startswith("exit"):
             break
+        elif command == "" or command.isspace():
+            continue
         result = ssp.get_parser().parse(command)
         for p in result:
             path = get_full_path(p.getCommand().getProgram())
+            if path is None:
+                print("{}: command not found".format(p.getCommand().getProgram()))
+                break
             arch = get_arch(path)
             print(p)
             print("path:{} | arch:{}".format(path, arch))
+        args = shlex.split(command)
+        try:
+            output = subprocess.check_output(args, universal_newlines=True)
+        except subprocess.CalledProcessError as e:
+            print("Cos nie tal")
 
 if __name__ == '__main__':
     main()
